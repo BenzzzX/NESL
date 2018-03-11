c0b457244aaed50f6003f59ab0aaa93bd4d670e3
BenzzZX <BenzzZX@outlook.com>
2018-03-11 18:57:19 +0800

    Plugin test

---
+++ ESLConsole/Declare.h
@@ -0,0 +1,43 @@
+#pragma once
+
+
+#include "../NESL/NESL.h"
+
+
+#pragma region States
+
+using GEntities = ESL::Entities;
+
+struct GFrame { uint64_t total; };
+GLOBAL_STATE(GFrame);
+
+struct GCanvas { };
+GLOBAL_STATE(GCanvas);
+
+struct EAppearance { char v; }; //显示的字符
+ENTITY_STATE(EAppearance, Vec);
+
+struct ELocation { int32_t x, y; }; //位置
+ENTITY_STATE(ELocation, Vec);
+
+struct EVelocity { int32_t x, y; }; //移动速度
+ENTITY_STATE(EVelocity, Vec);
+
+struct ELifeTime { int32_t n; }; //计时死亡
+ENTITY_STATE(ELifeTime, Vec);
+
+struct ESpawner { int32_t life; }; //生成计时死亡的实体
+ENTITY_STATE(ESpawner, Hash);
+
+#pragma endregion
+
+
+auto Logic_Spawn(const ESpawner& sp, const ELocation& loc, GEntities& entities,
+	ESL::State<ELifeTime>& lifetimes, ESL::State<ELocation>& locations,
+	ESL::State<EAppearance>& appearances);
+
+auto Logic_LifeTime(ELifeTime& life, ESL::Entity self, const GEntities& entities);
+
+auto Logic_Move(ELocation& loc, EVelocity& vel, const GCanvas& canvas);
+
+auto Logic_Draw(const ELocation& loc, const EAppearance& ap, const GCanvas& canvas);
--- ESLConsole/Game.cpp
+++ ESLConsole/Game.cpp
@@ -1,48 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <chrono>
 #include <thread>
-
-
-#include "../NESL/NESL.h"
-
-#pragma region States
-
-using GEntities = ESL::Entities;
-
-struct GFrame { uint64_t total; };
-GLOBAL_STATE(GFrame);
-
-struct GCanvas { };
-GLOBAL_STATE(GCanvas);
-
-struct EAppearance { char v; }; //显示的字符
-ENTITY_STATE(EAppearance, Vec);
-
-struct ELocation { int32_t x, y; }; //位置
-ENTITY_STATE(ELocation, Vec);
-
-struct EVelocity { int32_t x, y; }; //移动速度
-ENTITY_STATE(EVelocity, Vec);
-
-struct EGravity { int32_t g; }; //移动速度
-ENTITY_STATE(EGravity, Vec);
-
-struct ELifeTime { int32_t n; }; //计时死亡
-ENTITY_STATE(ELifeTime, Vec);
-
-struct ESpawner { int32_t life; }; //生成计时死亡的实体
-ENTITY_STATE(ESpawner, Hash);
-
-#pragma endregion
-
+#include "Declare.h"
+#include <io.h>
 
 #pragma region Logics
 
-auto Logic_Draw = [](const ELocation& loc, const EAppearance& ap, const GCanvas& canvas)
+auto Logic_Draw(const ELocation& loc, const EAppearance& ap, const GCanvas& canvas)
 {
 	std::cout << loc.x << " " << loc.y << " " << ap.v << "\n";
-};
+}
 
-auto Logic_Move = [](ELocation& loc, EVelocity& vel, const GCanvas& canvas)
+auto Logic_Move(ELocation& loc, EVelocity& vel, const GCanvas& canvas)
 {
 	loc.x += vel.x;
 	loc.y += vel.y;
@@ -66,12 +35,7 @@ auto Logic_Move = [](ELocation& loc, EVelocity& vel, const GCanvas& canvas)
 		vel.y = std::abs(vel.y);
 		loc.y = 0 - loc.y;
 	}
-};
-
-auto Logic_Gravity = [](EVelocity& vel, const EGravity& gravity)
-{
-	vel.y -= gravity.g;
-};
+}
 
 auto Logic_LifeTime(ELifeTime& life, ESL::Entity self, const GEntities& entities)
 {
@@ -96,8 +60,7 @@ auto Logic_Spawn(const ESpawner& sp, const ELocation& loc, GEntities& entities,
 #pragma endregion
 
 auto Init(GEntities& entities, ESL::State<ESpawner>& spawners, ESL::State<ELocation>& locations,
-	ESL::State<EVelocity>& velocities, ESL::State<EGravity>& gravities, 
-	ESL::State<EAppearance>& appearances)
+	ESL::State<EVelocity>& velocities, ESL::State<EAppearance>& appearances)
 {
 	auto res = entities.TrySpawn();
 	if (res.has_value())
@@ -105,10 +68,45 @@ auto Init(GEntities& entities, ESL::State<ESpawner>& spawners, ESL::State<ELocat
 		auto e = res.value();
 		locations.Create(e, 5, 5);
 		appearances.Create(e, '@');
-		gravities.Create(e, 1);
 		velocities.Create(e, 5, 0);
 		spawners.Create(e, 1);
 	}
+}
+
+void Register(ESL::States& st, ESL::LogicGraphBuilder& graphBuilder)
+{
+	st.Create<EVelocity>();
+	st.Create<ELocation>();
+	st.Create<ELifeTime>();
+	st.Create<EAppearance>();
+	st.Create<ESpawner>();
+	graphBuilder.Create(Logic_Draw, "Draw");
+	graphBuilder.Create(Logic_Move, "Move").Depends("Draw", "Spawn");
+	graphBuilder.Create(Logic_LifeTime, "LifeTime").Depends("Spawn");
+	graphBuilder.Create(Logic_Spawn, "Spawn").Depends("Draw");
+}
+
+using fRegister = void(*)(ESL::States&, ESL::LogicGraphBuilder&);
+
+void RegisterPlugins(ESL::States& st, ESL::LogicGraphBuilder& graphBuilder)
+{
+	char findDir[] = "plugins/*.dll";
+	char pluginDir[100] = "plugins/";
+	intptr_t handle;
+	_finddata_t findData;
+
+	handle = _findfirst(findDir, &findData);
+	if (handle == -1) return;
+	do 
+	{
+		strcpy(pluginDir + 8, findData.name);
+		std::cout << pluginDir << "\n";
+		HMODULE hModule = LoadLibrary(pluginDir);
+		fRegister reg = reinterpret_cast<fRegister>(GetProcAddress(hModule, "Register"));
+		reg(st, graphBuilder);
+		//FreeLibrary(hModule);
+	} while (_findnext(handle, &findData) == 0);
+	_findclose(handle);
 }
 
 
@@ -117,31 +115,22 @@ int main()
 #pragma region ESL
 
 	ESL::States st;
-	st.Create<EVelocity>();
-	st.Create<ELocation>();
-	st.Create<ELifeTime>();
-	st.Create<EGravity>();
-	st.Create<EAppearance>();
-	st.Create<ESpawner>();
+	ESL::LogicGraphBuilder graphBuilder(st);
 	GFrame &frame = st.Create<GFrame>().Raw();
 	GCanvas &canvas = st.Create<GCanvas>().Raw();
-	ESL::Dispatch(st, Init);
 
-	ESL::LogicGraphBuilder graphBuilder(st);
-	graphBuilder.Create(Logic_Draw, "Draw");
-	graphBuilder.Create(Logic_Move, "Move").Depends("Draw", "Spawn");
-	graphBuilder.Create(Logic_LifeTime, "LifeTime").Depends("Spawn");
-	graphBuilder.Create(Logic_Gravity, "Gravity").Depends("Move");
-	graphBuilder.Create(Logic_Spawn, "Spawn").Depends("Draw");
+	Register(st, graphBuilder);
+	ESL::Dispatch(st, Init);
+	RegisterPlugins(st, graphBuilder);
+
 	graphBuilder.Compile();
 	graphBuilder.ExportGraphviz("test.gv");
 	ESL::LogicGraph logicGraph;
 	graphBuilder.Build(logicGraph);
+
 #pragma endregion
 
 #pragma region GameLoop
-
-
 
 	while (true) {
 		using namespace std::chrono;
